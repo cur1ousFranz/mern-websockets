724f0ea12a8e28437a79712e4de20b9671089f01
cur1ousFranz <xfranjeff@gmail.com>
2023-06-05 22:51:23 +0800

    socket connection / joining room / broadcast event

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,28 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
+const { instrument } = require('@socket.io/admin-ui')
 
-app.listen(process.env.PORT, () => {
-    console.log('listening to port: ', process.env.PORT)
+const io = require('socket.io')(4000, {
+    cors: {
+        origin: ['http://localhost:3000', 
+        'https://admin.socket.io']
+    }
 })
+
+io.on('connection', socket => {
+    // listen to event
+    socket.on('send-message', (message, room) => {
+        if (room === '') {
+            // send exclude the user who creates message
+            socket.broadcast.emit('recieve-message', message)
+        } else {
+            // send message to client with specified room
+            socket.to(room).emit('recieve-message', message)
+        }
+    })
+
+    socket.on('join-room', (room, callback) => {
+        socket.join(room)
+        callback('Joined')
+    })
+})
+
+instrument(io, { auth: false })
